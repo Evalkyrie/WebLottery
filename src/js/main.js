e7ee6f1bd9a4d8dacbeb7370abded29ed1fdc90e
clynch37 <camlynch314@gmail.com>
2026-06-03 14:27:37 -0400

    basic functionality

--- src/js/main.js
+++ src/js/main.js
@@ -17,12 +17,16 @@ var x = 5;
 
 // Variables for the UI Elements (radio buttons, textboxes)
 var picks = document.getElementById('picks');
-
-
+var result = document.getElementById("result");
+var outcome = document.getElementById("outcome");
+var basic = document.getElementById("Basic");
+var standard = document.getElementById("Standard");
+var mega = document.getElementById("Mega");
 
 document.getElementById('PLAY').addEventListener('click', () => {
     won = 0;
     loss = 0;
+    ticket = getTicket();
     pickList = picks.value.split(' ');
     picklength = Object.keys(pickList).length;
     //alert('I got clicked!');
@@ -36,15 +40,42 @@ document.getElementById('PLAY').addEventListener('click', () => {
             }
             // alert("comparing " + pickList[j] + " with " + results[i]);
         }
-        alert('giving you ' + results[i]);
     }
-    alert('you won ' + won);
+    winnings += won;
+    loss = ticketPrices[ticket];
+    spendings += loss;
+    result.innerHTML = results.join(" ");
+
+    if (won > loss)
+    {
+        outcome.innerHTML = "You Won $" + (won - loss) + "!";
+    }
+    else if (won == loss)
+    {
+        outcome.innerHTML = "You Broke Even!";
+    }
+    else
+    {
+        outcome.innerHTML = "You Lost $" + loss;
+    }
+
 })
 
 var radios = document.forms["ticketType"].elements["ticket"];
-for(radio in radios) {
-    radios[radio].onclick = function() {
-        alert(this.value);
-        ticket = this.value;
+function getTicket() {
+    if (basic.checked) {
+        return 0;
+    } else if (standard.checked) {
+        return 1;
+    } else if (mega.checked) {
+        return 2
     }
 }
+
+// var radios = document.forms["ticketType"].elements["ticket"];
+// for(radio in radios) {
+//     radios[radio].onclick = function() {
+//         alert(this.value);
+//         ticket = this.value;
+//     }
+// }
